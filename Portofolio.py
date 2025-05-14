7ead195a680b40eea99c22f3e599a20a4444197b
German Jacome <uwtgm@student.kit.edu>
2025-05-14 21:06:04 +0200

    elimine Main

--- Portofolio.py
+++ Portofolio.py
@@ -5,9 +5,23 @@ import pandas as pd
 
 tickers = ['AAPL', 'LMT', 'ALHC', 'MC.PA', 'PBR']
 
-data = yf.download(tickers, start="2020-01-01", auto_adjust=False)['Adj Close']
-log_returns = np.log(data / data.shift(1)).dropna()
+data = yf.download(tickers, start="2020-01-01", end = "2025-05-14")
 
-print(data.head())
-print(log_returns.head())
-print("Este es un cambio de prueba")
+simple_returns = data.pct_change().dropna()
+
+def jarque_bera_testStatistic(sample):
+    n = len(sample)
+    mean = np.mean(sample)
+    standard_deviation = np.std(sample)
+
+    skewness = np.mean(((sample - mean) / standard_deviation)**3)
+    kurtosis = np.mean(((sample - mean) / standard_deviation)**4)
+
+    jb_value = (n / 6) * (skewness**2 + ((kurtosis - 3)**2) / 4)
+
+    return jb_value, skewness, kurtosis
+
+for ticker in simple_returns.columns:
+    serie = simple_returns[ticker].dropna()
+    jb_value, skewness, kurtosis = jarque_bera_testStatistic(serie)
+    (f"{ticker}: JB_Value = {jb_value:.2f}, skewness = {skewness:.2f}, kurtosis = {kurtosis:.2f}")

6f92a3743749c3e3e8cb0b2077645ad26d851f94
rocas2704 <rodrigocastilla27@gmail.com>
2025-05-16 15:32:56 +0200

    listo

--- LogicAlgortihm.py
+++ LogicAlgortihm.py
@@ -62,12 +62,29 @@ for i in range(n):
     sigma_value = (w_i.T @ Cov_matrix @ w_i)[0, 0]
     sigma_MV[i] = np.sqrt(sigma_value)
 
+
+rf = 0.02
+SR_MV = (mu_MV - rf)/sigma_MV
+
+TP_index = np.argmax(SR_MV)
+
+w_TP = w_MV[TP_index,:]
+mu_TP = mu_MV[TP_index,0]
+sigma_TP = sigma_MV[TP_index,0]
+SR_TP = (mu_TP - rf)/sigma_TP
+
 # Visualización
+sigma_cml = np.linspace(0, max(sigma_MV), 100)
+mu_cml = rf + SR_TP * sigma_cml
+
 individual_sigma = np.sqrt(np.diag(Cov_matrix))
 individual_mu = mu[:, 0]
 
 plt.figure(figsize=(12, 7))
-plt.plot(sigma_MV, mu_MV, color='blue', label='Minimum Variance Frontier (Corregida)')
+plt.plot(sigma_MV, mu_MV, color='blue', label='Minimum Variance Frontier ')
+plt.plot(sigma_cml, mu_cml, color='gray', linestyle='--', label='Capital Market Line ')
+plt.scatter(sigma_TP, mu_TP, color='red', s=100, label='Tangency Portfolio (TP)', marker='*')
+plt.scatter(0, rf, color='green', s=100, label='Risk-Free Rate ($R_f$)', marker='o')
 for i, ticker in enumerate(tickers):
     plt.scatter(individual_sigma[i], individual_mu[i], s=50, label=ticker, marker='o', color='black')
     plt.text(individual_sigma[i], individual_mu[i], ticker, fontsize=9, ha='right', va='bottom')
@@ -77,3 +94,7 @@ plt.ylabel('Expected Return (Annual)')
 plt.title("Minimum Variance Frontier (Corregida)")
 plt.legend()
 plt.show()
+
+print(mu)
+
+#Capital Market Line
